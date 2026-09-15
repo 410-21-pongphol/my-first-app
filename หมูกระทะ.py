51030f8ccf40c50c9e1fa7dc35afc8d4a975e073
410-21-pongphol <54453@yupparaj.ac.th>
2026-09-15 13:18:20 +0700

    Update หมูกระทะ.py

--- หมูกระทะ.py
+++ หมูกระทะ.py
@@ -1,9 +1,8 @@
 import streamlit as st
-st.title("🛒แอปพลิเคชั่นคำนวณราคาสินค้ารวม VAT 15%")
+st.title("🛒แอปพลิเคชั่นคำนวณราคาสินค้ารวม VAT 10%")
 price = st.number_input("กรอกราคาสินค้า (บาท):", value=0.0)
-vat = price * 0.15
+vat = price * 0.10
 net_price = price - vat
-st.header(f"• ภาษีมูลค่าเพิ่ม (VAT 15%): **{vat:.2f}** บาท")
+st.header(f"• ภาษีมูลค่าเพิ่ม (VAT 10%): **{vat:.2f}** บาท")
 st.header(f"• ราคาสุทธิ: {net_price:.2f} บาท")
 st.divider()
-st.write("นายปองพล เปาวะนานนท์ เลขที่ 21  ม.4/10")
