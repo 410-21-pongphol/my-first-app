bcd64ff0a8361b01bf9aa6a2c6cd201f3bf09121
410-21-pongphol <54453@yupparaj.ac.th>
2026-09-22 13:31:01 +0700

    Update หมูกระทะปัญญาประดิษ.py

--- หมูกระทะปัญญาประดิษ.py
+++ หมูกระทะปัญญาประดิษ.py
@@ -26,4 +26,11 @@ st.header(f"• ราคาสุทธิ: **{net_price:.2f}** บาท")
 
 st.divider()
 
-st.write("นายสมชาย ใจดี เลขที่ 1 ม.4/1")
+import streamlit as st
+st.title("🛒แอปพลิเคชั่นคำนวณราคาสินค้าพร้อมส่วนลด 10%")
+price = st.number_input("กรอกราคาสินค้า (บาท):", value=0.0)
+discount = price * 0.10
+net_price = price - discount
+st.header(f"• ภาษีมูลค่าเพิ่ม (VAT 10%): **{discount:.2f}** บาท")
+st.header(f"• ราคาสุทธิ: {net_price:.2f} บาท")
+st.divider()
